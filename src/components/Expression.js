c4cae629df9c6b48b27be2066ab0d7116a9c091c
hieutrungtran2205 <trunghieu22052k@gmail.com>
2022-03-17 09:46:08 +0700

    add Interval

--- src/components/Expression.js
+++ src/components/Expression.js
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import '../App.css';
-// import useRandom from '../hooks/useRandom';
+import 'antd/dist/antd.css';
+import { Progress } from 'antd';
 
 function Expression() {
     const [expression, setExpression] = useState()
@@ -9,6 +10,7 @@ function Expression() {
     const [color, setColor] = useState('green')
     const [highScore, setHighScore] = useState(JSON.parse(localStorage.getItem('highScore') ?? 0))
     const [isOver, setIsOver] = useState(false)
+    const [percent, setPercent] = useState(0)
 
     const getRandomColor = () => {
         const colors = ['green', 'red', 'orange', 'blue', 'brown', 'pink', 'black']
@@ -38,10 +40,10 @@ function Expression() {
         return eval(`${num1} ${op} ${num2}`)
     }, [op])
 
-    const getFakeResult = (num1, num2, result) => {
+    const getFakeResult = useCallback((num1, num2, result) => {
         let fakeResult = getRandomNumber(num1, num2);
         return result === fakeResult ? getFakeResult(num1, num2, result) : fakeResult
-    }
+    }, [])
 
     const getRandomResult = useCallback((num1, num2) => {
         const randomResult = Math.random() >= 0.5;
@@ -49,7 +51,7 @@ function Expression() {
         const result = getResult(num1, num2);
         const fakeResult = getFakeResult(num1, num2, result);
         return randomResult ? result : fakeResult;
-    }, [getResult])
+    }, [getFakeResult, getResult])
 
     const getRandomExpression = useCallback(() => {
         const num1 = getRandomNumber();
@@ -64,10 +66,11 @@ function Expression() {
             setExpression(getRandomExpression())
             setScore(score => score + 1)
             setColor(notRepeatColor())
+            setHighScore(score)
+            setPercent(0)
         } else {
             setExpression("game over")
             setScore(0)
-            console.log("s", score);
             setIsOver(true)
         }
     }, [getRandomExpression, isTrue, notRepeatColor, score])
@@ -77,42 +80,61 @@ function Expression() {
             setExpression(getRandomExpression())
             setScore(score => score + 1)
             setColor(notRepeatColor())
+            setPercent(0)
+            setHighScore(score)
         } else {
             setExpression("game over")
             setScore(0)
             setIsOver(true)
+            setPercent(0)
         }
     }, [getRandomExpression, isTrue, notRepeatColor, score])
-
-    const oldScore = JSON.parse(localStorage.getItem('highScore'))
-    console.log(oldScore);
-    if (oldScore <= highScore) {
-        localStorage.setItem('highScore', JSON.stringify(highScore))
-    }
 
     const playAgain = () => {
         setExpression(getRandomExpression())
         setScore(0)
         setHighScore(JSON.parse(localStorage.getItem('highScore')))
         setIsOver(false)
+        setPercent(0)
     }
+
+    useEffect(() => {
+        const oldScore = JSON.parse(localStorage.getItem('highScore'))
+        console.log("o", oldScore);
+        if (oldScore <= highScore) {
+            localStorage.setItem('highScore', JSON.stringify(highScore))
+        }
+    }, [highScore])
+
+    useEffect(() => {
+        const myInterval = setInterval(() => {
+            setPercent(percent => percent + 1)
+        }, 50)
+        if (percent >= 100) {
+            clearInterval(myInterval)
+            setIsOver(true)
+            setHighScore(highScore)
+        }
+
+        return () => clearInterval(myInterval)
+    }, [highScore, percent])
 
     useEffect(() => {
         setExpression(getRandomExpression())
     }, [])
 
-
-    console.log(isOver);
     return (
         <div className='content' style={{ backgroundColor: `${color}` }}>
             <span className='score'>Score: {score}</span>
             <div className='expression'>
                 <h1>{expression}</h1>
             </div>
-            {isOver ?
-                <button className='btn playAgain' onClick={playAgain}>Play Again</button> : ''
+            {isOver
+                ? <button className='btn playAgain' onClick={playAgain}>Play Again</button>
+                : <Progress type='circle' className='progress' percent={percent} />
             }
-            <span className='highScore'>High Score: {highScore}</span>
+
+            <span className='highScore'>High Score: {JSON.parse(localStorage.getItem('highScore'))}</span>
             <div className='buttons'>
                 <button className='btn true' onClick={onClickTrue}>True</button>
                 <button className='btn false' onClick={onClickFalse}>False</button>
